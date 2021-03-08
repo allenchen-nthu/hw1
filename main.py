30188b17d425f5c7ca0e33a21d0d2e929b05f2b5
allenchen-nthu <79574385+allenchen-nthu@users.noreply.github.com>
2021-03-08 23:13:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import csv
 # Part. 2
 #=======================================
 # Read cwb weather data
-cwb_filename = '108061105.csv'
+cwb_filename = 'sample_input.csv'
 data = []
 header = []
 with open(cwb_filename) as csvfile:
@@ -23,117 +23,117 @@ result = []
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0A880" as a list.
 target_data = list(filter(lambda item: item['station_id'] == 'C0A880', data))
-#initialize status of full available values
-status = 1
+#initialize the number of non available values
+no_value = 0
 #initialize the sum of PRES
 summation = 0
 #discriminate the value
 for i in range(len(target_data)):
     if (target_data[i]["PRES"] == '-99.000'):
-        status = 0
+        no_value = no_value + 1
     elif (target_data[i]["PRES"] == '-999.000'):
-        status = 0
+        no_value = no_value + 1
     else:
         summation += float(target_data[i]["PRES"])
-#calculating mean
-mean = summation / len(target_data)
 #put the result into the list
-if (status == 1):
-    result.append(['C0A880', mean])
+if (no_value == len(target_data)):
+    result.append(['C0A880', 'None'])
 else:
-    result.append(['C0A880', None])
+    #calculating mean
+    mean = summation / (len(target_data) - no_value)
+    result.append(['C0A880', mean])
 
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0F9A0" as a list.
 target_data = list(filter(lambda item: item['station_id'] == 'C0F9A0', data))
-#initialize status of full available values
-status = 1
+#initialize the number of non available values
+no_value = 0
 #initialize the sum of PRES
 summation = 0
 #discriminate the value
 for i in range(len(target_data)):
     if (target_data[i]["PRES"] == '-99.000'):
-        status = 0
+        no_value = no_value + 1
     elif (target_data[i]["PRES"] == '-999.000'):
-        status = 0
+        no_value = no_value + 1
     else:
         summation += float(target_data[i]["PRES"])
-#calculating mean
-mean = summation / len(target_data)
 #put the result into the list
-if (status == 1):
-    result.append(['C0F9A0', mean])
+if (no_value == len(target_data)):
+    result.append(['C0F9A0', 'None'])
 else:
-    result.append(['C0F9A0', None])
+    #calculating mean
+    mean = summation / (len(target_data) - no_value)
+    result.append(['C0F9A0', mean])
 
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0R190" as a list.
 target_data = list(filter(lambda item: item['station_id'] == 'C0R190', data))
-#initialize status of full available values
-status = 1
+#initialize the number of non available values
+no_value = 0
 #initialize the sum of PRES
 summation = 0
 #discriminate the value
 for i in range(len(target_data)):
     if (target_data[i]["PRES"] == '-99.000'):
-        status = 0
+        no_value = no_value + 1
     elif (target_data[i]["PRES"] == '-999.000'):
-        status = 0
+        no_value = no_value + 1
     else:
         summation += float(target_data[i]["PRES"])
-#calculating mean
-mean = summation / len(target_data)
 #put the result into the list
-if (status == 1):
-    result.append(['C0R190', mean])
+if (no_value == len(target_data)):
+    result.append(['C0R190', 'None'])
 else:
-    result.append(['C0R190', None])
+    #calculating mean
+    mean = summation / (len(target_data) - no_value)
+    result.append(['C0R190', mean])
 
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0G640" as a list.
 target_data = list(filter(lambda item: item['station_id'] == 'C0G640', data))
-#initialize status of full available values
-status = 1
+#initialize the number of non available values
+no_value = 0
 #initialize the sum of PRES
 summation = 0
 #discriminate the value
 for i in range(len(target_data)):
     if (target_data[i]["PRES"] == '-99.000'):
-        status = 0
+        no_value = no_value + 1
     elif (target_data[i]["PRES"] == '-999.000'):
-        status = 0
+        no_value = no_value + 1
     else:
         summation += float(target_data[i]["PRES"])
-#calculating mean
-mean = summation / len(target_data)
 #put the result into the list
-if (status == 1):
-    result.append(['C0G640', mean])
+if (no_value == len(target_data)):
+    result.append(['C0G640', 'None'])
 else:
-    result.append(['C0G640', None])
+    #calculating mean
+    mean = summation / (len(target_data) - no_value)
+    result.append(['C0G640', mean])
 
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0X260" as a list.
 target_data = list(filter(lambda item: item['station_id'] == 'C0X260', data))
-#initialize status of full available values
-status = 1
+#initialize the number of non available values
+no_value = 0
 #initialize the sum of PRES
 summation = 0
 #discriminate the value
 for i in range(len(target_data)):
     if (target_data[i]["PRES"] == '-99.000'):
-        status = 0
+        no_value = no_value + 1
     elif (target_data[i]["PRES"] == '-999.000'):
-        status = 0
+        no_value = no_value + 1
     else:
         summation += float(target_data[i]["PRES"])
-#calculating mean
-mean = summation / len(target_data)
 #put the result into the list
-if (status == 1):
-    result.append(['C0X260', mean])
+if (no_value == len(target_data)):
+    result.append(['C0X260', 'None'])
 else:
-    result.append(['C0X260', None])
+    #calculating mean
+    mean = summation / (len(target_data) - no_value)
+    result.append(['C0X260', mean])
 #=======================================
 
 # Part. 4
